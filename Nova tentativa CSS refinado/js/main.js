193002ea93bae0599b7d2420802677c9f8f0d387
Luccas <luccasdamasceno@gmail.com>
2021-10-09 22:22:50 -0300

    Projetos iniciados

---
+++ Nova tentativa CSS refinado/js/main.js
@@ -0,0 +1,9 @@
+// $('.slider').slick({
+//     dots: false,
+//     infinite: true,
+//     speed: 150,
+//     slidestoshow: 1,
+//     adaptiveHeight: true,
+//     autoplay: true,
+//     autoplaySpeed: 1000
+// });
---
+++ começo do html e css/Projeto 1/js/main.js
@@ -0,0 +1,9 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidestoshow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+});
